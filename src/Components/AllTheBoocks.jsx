3c4a62f1877a9b4c1f83d735eb8037b86c1f19ef
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-04-01 16:11:08 +0200

    adds modified

--- src/Components/AllTheBoocks.jsx
+++ src/Components/AllTheBoocks.jsx
@@ -60,9 +60,9 @@ class AllTheBoocks extends Component {
           </Row>
 
           <Row className="d-flex justify-content-center">
-            {this.state.ActiveBooks.slice(0, 6).map((libro) => (
+            {this.state.ActiveBooks.slice(0, 12).map((libro) => (
               <Col
-                className="col-xl-2 col-lg-3 col-md-4 col-xs-6 g-3  "
+                className="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12 g-3  "
                 key={libro.asin}
               >
                 <Card className="d-flex flex-column h-100 trasformazione">
