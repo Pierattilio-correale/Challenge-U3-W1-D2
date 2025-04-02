245aed75aa8b4d47ef878a0eba12b3fcdd363f1c
Pierattilio-correale <pierattiliocorreale@yahoo.it>
2025-04-02 16:02:46 +0200

    adds extra

--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -1,14 +1,50 @@
 import SingleBook from "./SingleBook";
+import Form from "react-bootstrap/Form";
+import { Card, Button, Col, Container, Row } from "react-bootstrap";
+import { Component } from "react";
 
-const BookList = function (props) {
-  return (
-    <>
-      <h3>esercizio dei multipli libri</h3>
-      {props.listBook.map((libro) => (
-        <SingleBook key={libro.asin} oneBook={[libro]} />
-      ))}
-    </>
-  );
-};
+class BookList extends Component {
+  state = {
+    formPower: {
+      change: "",
+    },
+  };
+  render() {
+    return (
+      <>
+        <h3>esercizio dei multipli libri</h3>
+        <Container>
+          <Row className="d-flex justify-content-center">
+            <Col xs={12} md={8} lg={6}>
+              <Form>
+                <Form.Group className="mb-3 ">
+                  <Form.Label>Cerca qui il tuo libro preferito!</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={this.state.formPower.change}
+                    onChange={(e) => {
+                      this.setState({
+                        formPower: {
+                          ...this.state.formPower,
+                          change: e.target.value,
+                        },
+                      });
+                    }}
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
 
+        {this.props.listBook.map((libro) => (
+          <SingleBook key={libro.asin} oneBook={[libro]} />
+        ))}
+      </>
+    );
+  }
+}
 export default BookList;
